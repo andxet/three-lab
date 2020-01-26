9af5b68077bfab984b1e19c3241c5146e6c1007f
Andrea Peretti <and.xet@gmail.com>
2020-01-26 20:37:28 +0100

    simplified project

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import App from './app'
-
-var solarSystem = new App();
-solarSystem.Run();
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mode: 'development',
-    devServer: {
-        contentBase: require('path').join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-        }
-    };
